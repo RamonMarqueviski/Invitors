676f28ac71f66e9fb116add63c51fbd7c7b60c94
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2022-06-13 22:14:06 -0300

    oioioi

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-54-147-33-38.compute-1.amazonaws.com',
-  username: 'piujgzabongwkf',
-  password: '0c910cd6be4be79676fe9f63b343b0e00e19cedbb41c1f99a972102d11f89a55',
-  database: 'dcseb8r2a63ou7',
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   define: {
     timestamps: true,
     underscored: true,
--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,12 @@
 const Sequelize = require('sequelize');
 
+const dbConfig = require('../config/database')
+
 const Clube = require('../models/Clube');
 const Atleta = require('../models/Atleta');
 const Competicoes = require('../models/Competicao');
 
-const connection = new Sequelize(process.env.DATA_URL,{
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  define: {
-    timestamps: true,
-    underscored: true,
-  }
-});
+const connection = new Sequelize(dbConfig);
 
 connection
   .authenticate()
