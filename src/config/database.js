fd62ffc0e43f7e5c96bd94daa7b18c1c9bbcee38
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2022-06-21 19:35:50 -0300

    lalala

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,17 @@
 module.exports = {
-  dialect: 'postgres',
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
+  dialect: "postgres",
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   define: {
     timestamps: true,
     underscored: true,
-  }
-
-}
-
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+    },
+  },
+};
