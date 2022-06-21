269a1d8076161a95b2323ac69cbea0d600290a30
João Elias Engelbrecht Kayser <jeliasek@gmail.com>
2022-06-21 19:51:53 -0300

    ola

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "postgres",
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
