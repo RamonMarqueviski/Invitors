5a4c6e6333d311d8d8acea5e4c8ee8bb81f075e2
Ramon Vinicius Marqueviski <68443821+RamonMarqueviski@users.noreply.github.com>
2022-06-27 10:36:54 -0300

    Cors

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+
 
 require('./database');
 const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.timeout = 100000;
