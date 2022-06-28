285700065d531644a82f2a580f86677b7a4eae44
Ramon Vinicius Marqueviski <68443821+RamonMarqueviski@users.noreply.github.com>
2022-06-28 09:53:02 -0300

    Ajuste Status

--- src/controllers/StatusController.js
+++ src/controllers/StatusController.js
@@ -1,42 +1,41 @@
 //Crud de status
 
-const status = require('../models/Status');
+const Status = require("../models/State");
 
 module.exports = {
   async index(req, res) {
     //Procura todos os status
-        const status = await status.findAll();
-        return res.json(status);
-
+    const status = await Status.findAll();
+    return res.json(status);
   },
   async store(req, res) {
     //Cria um novo status
     const { nome } = req.body;
-    const status = await status.create({ nome });
+    const status = await Status.create({ nome });
     return res.json(status);
-    
   },
   async update(req, res) {
     //Atualiza um status
-      const { nome } = req.body;
-      const { id } = req.params;
-      const status = await status.findByPk(id);
-      if (!status) {
-          return res.status(400).json({ error: "Status não encontrado!" });
-      }
-      await status.update({ nome });
-      return res.json(status);
+    const { nome } = req.body;
+    const { id } = req.params;
+    const status = await Status.findByPk(id);
+    if (!status) {
+      return res.status(400).json({ error: "Status não encontrado!" });
+    }
+    await Status.update({ nome }, { where: { id: id } });
+    const statusAtualizado = await Status.findByPk(id);
+    return res.json(statusAtualizado);
   },
   async delete(req, res) {
     //Deleta um status
-      const { id } = req.params;
-      const status = await status.findByPk(id);
-      if (!status) {
-            return res.status(400).json({ error: "Status não encontrado!" });
-      }
-      await status.destroy({
-          where: { id: id },
-      });
-        return res.json({ retorno: "Status deletado com sucesso!" });
+    const { id } = req.params;
+    const status = await Status.findByPk(id);
+    if (!status) {
+      return res.status(400).json({ error: "Status não encontrado!" });
+    }
+    await Status.destroy({
+      where: { id: id },
+    });
+    return res.json({ retorno: "Status deletado com sucesso!" });
   },
 };
